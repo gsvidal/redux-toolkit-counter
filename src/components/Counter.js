23626ab1fa27c86e3f2bed6548dcf5d6301facdc
Gonzo <gonzalovidal.dev@gmail.com>
2023-01-06 22:32:01 -0500

    Dispatch actions(type increment and decrement)  which are forwarded to counterReducer function

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,27 @@
 import classes from './Counter.module.css';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 const Counter = () => {
+  const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
+
+  const handleIncrement = () => {
+    dispatch({ type: 'increment' });
+  };
+  const handleDecrement = () => {
+    dispatch({ type: 'decrement' });
+  };
 
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={handleIncrement}>Increment 1</button>
+        <button onClick={handleDecrement}>Decrement 1</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
